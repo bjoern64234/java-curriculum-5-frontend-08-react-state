3101582631ecb4202449548e602ea4b0072ea21b
bjoern <b.irion@wtnet.de>
2026-06-16 10:52:49 +0200

    Fix typo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 
     return (
         <>
-            <h2>Welcome to the rick and mortey app ...</h2>
+            <h2>Welcome to the rick and morty app ...</h2>
             <p>Navigate to to search ...</p>
         </>
     )
